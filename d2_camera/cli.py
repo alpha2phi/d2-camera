c15b74eb6a9bf2c2da65053d9b454ae2ddaa2ec1
Meng Wang <mengwangk@gmail.com>
2021-03-05 09:31:21 +0800

    Initial commit

--- d2_camera/cli.py
+++ d2_camera/cli.py
@@ -4,7 +4,6 @@ import fire
 
 
 def cli():
-    # Don't instantiate imagine if the user just wants help.
     if any("--help" in arg for arg in sys.argv):
         print("Type `d2 --help` for usage info.")
         sys.exit()
