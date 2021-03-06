b8938bed5336d314dbe606b734088674219726c7
Meng Wang <mengwangk@gmail.com>
2021-03-06 22:09:56 +0800

    Initial commit

--- d2_camera/d2.py
+++ d2_camera/d2.py
@@ -4,8 +4,9 @@ import multiprocessing as mp
 import os
 import random
 import sys
+import time
 
-import cv2 as cv
+import cv2
 import detectron2
 from detectron2 import model_zoo
 from detectron2.config import get_cfg
@@ -13,7 +14,7 @@ from detectron2.data import DatasetCatalog, MetadataCatalog
 from detectron2.engine import DefaultPredictor
 from detectron2.utils.logger import setup_logger
 from detectron2.utils.visualizer import Visualizer
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 import numpy as np
 import torch
 import torchvision
@@ -50,19 +51,22 @@ if not gpu:
 
 predictor = DefaultPredictor(cfg)
 
-detecting = False
-
 
 def video_capture():
     """
     Video capture.
     """
     # Default video capture
-    cap = cv.VideoCapture(0)
+    cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         logging.error("Cannot open camera")
         exit()
 
+    queue = Queue()
+    process = Process(target=object_detection, args=(queue,))
+    process.start()
+
+    cv2.namedWindow("frame1")
     while True:
         # Capture frame-by-frame
         ret, frame = cap.read()
@@ -72,40 +76,42 @@ def video_capture():
             logging.error("Can't receive frame. Exiting ...")
             break
 
-        cv.imshow("frame", frame)
+        # cv2.imshow("frame1", frame)
 
-        # if not detecting:
-        #     p = Process(target=object_detection, args=(frame,))
-        #     p.start()
+        if queue.empty():
+            queue.put(frame)
 
-        if cv.waitKey(1) == ord("q"):
+        if cv2.waitKey(1) == ord("q"):
+            queue.close()
+            queue.join_thread()
+            process.terminate()
             break
 
     # When everything done, release the capture
     cap.release()
-    cv.destroyAllWindows()
+    cv2.destroyAllWindows()
 
 
-def object_detection(frame):
+def object_detection(queue):
     """
     Real time object detection.
 
     :param frame Image: Image frame.
     """
-    global detecting
-    detecting = True
 
-    # Our operations on the frame come here
-    outputs = predictor(frame)
-    logging.debug(outputs["instances"].pred_classes)
-    logging.debug(outputs["instances"].pred_boxes)
+    cv2.namedWindow("frame2")
+    while True:
+        frame = queue.get()
 
-    # Display the resulting frame
-    v = Visualizer(
-        frame[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2
-    )
-    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-    cv.imshow("object", out.get_image()[:, :, ::-1])
+        # Our operations on the frame come here
+        outputs = predictor(frame)
+        logging.debug(outputs["instances"].pred_classes)
+        logging.debug(outputs["instances"].pred_boxes)
 
-    detecting = False
+        # Display the resulting frame
+        v = Visualizer(
+            frame[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2
+        )
+        out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+        cv2.imshow("frame2", out.get_image()[:, :, ::-1])
 
